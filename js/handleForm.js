2a0ce4e7da412401de20d76c9061851d74990f7a
Arkady Arkhangorodsky <arkady.arkhangorodsky@mail.utoronto.ca>
2015-02-24 21:55:15 -0500

    Added icons for external pages, added titles/types to posts

--- js/handleForm.js
+++ js/handleForm.js
@@ -9,8 +9,9 @@
       password = $("input.form-control").val();
       if (CryptoJS.SHA256(password).toString() == '5d6e0eecd32302adf4ad5e712f8f4e1db7cd9f9d37b0a1c1503aca6ee8ea7e44') {
         var myDataRef = new Firebase('https://arkadyark.firebaseio.com/' + selected);
+        var postTitle = $("input.titleField").val();
         var html = $("textarea.mdhtmlform-html").val();
-        myDataRef.push({post: html});
+        myDataRef.push({title: postTitle, post: html});
       } 
     });
   });
