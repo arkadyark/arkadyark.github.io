dc366821f173d8372e3de576d7849c31c2688e24
Arkady Arkhangorodsky <arkady.arkhangorodsky@mail.utoronto.ca>
2015-07-22 20:35:54 -0700

    Loaded partial network first to speed up time to first view

--- _site/visualizations/rap_network/attempt_5/graph.js
+++ _site/visualizations/rap_network/attempt_5/graph.js
@@ -81,67 +81,70 @@ function highlightNode(d, d3Selection) {
         } else {
             d3.select("#artist-number-label").style("display", "inline")
                 yeezyNumber.html(d.yeezy_number[artist_name])
-                $.each(d.yeezy_path[artist_name], function(key, value) {
-                    // Color edges on the way to the centre
-                    var edge = d3.select("#a" + CryptoJS.SHA1(value.to) + CryptoJS.SHA1(value.from))
-                        if (edge[0][0]) {
-                            // Look for the edge from a to b
-                            edge.attr("class", "path-to-centre")
-                                .attr("stroke-width", function(d) {
-                                    return Math.sqrt(d[3])
-                                })
-                            coloredEdges.push(edge)
-                        } else {
-                            // Look for the edge from b to a
-                            var edge = d3.select("#a" + CryptoJS.SHA1(value.from) + CryptoJS.SHA1(value.to))
-                                if (edge[0][0]) {
-                                    edge.attr("class", "path-to-centre")
-                                        .attr("stroke-width", function(d) {
-                                            return Math.sqrt(d[3])
-                                        })
-                                    coloredEdges.push(edge)
-                                } else {
-                                    // Edge not found (threshold too high)
-                                    dashedEdges.push({"source" : d3.select("#a" + CryptoJS.SHA1(value.from)), "target" : d3.select("#a" + CryptoJS.SHA1(value.to))})
-
-                                        dashedLink = vis.selectAll(".dashed-link")
-                                        .data(dashedEdges)
-                                        .enter().insert("path", ":first-child")
-                                        .attr("class", "dashed-link")
-                                        .attr("stroke-dasharray", "3,6")
-                                        .attr("stroke-width", "2")
-                                        .attr("d", function(d) {
-                                            var sourceX = d.source.attr("transform").split('(')[1].split(',')[0]
-                                                var sourceY = d.source.attr("transform").split('(')[1].split(',')[1].replace(')', '')
-                                                var targetX = d.target.attr("transform").split('(')[1].split(',')[0]
-                                                var targetY = d.target.attr("transform").split('(')[1].split(',')[1].replace(')', '')
-                                                return "M" + sourceX + "," + sourceY
-                                                + "L" + targetX + "," + targetY;
-                                        });
-                                }
-                        }
-
-                    // Color nodes on the way to the centre
-                    var intermediateNode = d3.select("#a" + CryptoJS.SHA1(value.to))
-                        intermediateNode.style("fill", function(d) {
-                            if (d.name != artist_name) {
-                                return d3.rgb(255, 106, 71);
+                if (yeezy_only) {
+                    d.yeezy_path = {"Kanye West": d.yeezy_path};
+                }
+            $.each(d.yeezy_path[artist_name], function(key, value) {
+                // Color edges on the way to the centre
+                var edge = d3.select("#a" + CryptoJS.SHA1(value.to) + CryptoJS.SHA1(value.from))
+                    if (edge[0][0]) {
+                        // Look for the edge from a to b
+                        edge.attr("class", "path-to-centre")
+                            .attr("stroke-width", function(d) {
+                                return Math.sqrt(d[3])
+                            })
+                        coloredEdges.push(edge)
+                    } else {
+                        // Look for the edge from b to a
+                        var edge = d3.select("#a" + CryptoJS.SHA1(value.from) + CryptoJS.SHA1(value.to))
+                            if (edge[0][0]) {
+                                edge.attr("class", "path-to-centre")
+                                    .attr("stroke-width", function(d) {
+                                        return Math.sqrt(d[3])
+                                    })
+                                coloredEdges.push(edge)
                             } else {
-                                return d3.rgb(255, 0, 0);
-                            }
-                        })
-                    intermediateNodes.push(intermediateNode)
+                                // Edge not found (threshold too high)
+                                dashedEdges.push({"source" : d3.select("#a" + CryptoJS.SHA1(value.from)), "target" : d3.select("#a" + CryptoJS.SHA1(value.to))})
 
-                        // Write out the path
-                        var sentence = "<p>"
-                        sentence += value.from
-                        sentence += " and "
-                        sentence += value.to
-                        sentence += " collaborated on "
-                        sentence += "<em>" + value.song + "</em>"
-                        sentence += "</p>"
-                        yeezyPath.append($(sentence))
-                })
+                                    dashedLink = vis.selectAll(".dashed-link")
+                                    .data(dashedEdges)
+                                    .enter().insert("path", ":first-child")
+                                    .attr("class", "dashed-link")
+                                    .attr("stroke-dasharray", "3,6")
+                                    .attr("stroke-width", "2")
+                                    .attr("d", function(d) {
+                                        var sourceX = d.source.attr("transform").split('(')[1].split(',')[0]
+                                            var sourceY = d.source.attr("transform").split('(')[1].split(',')[1].replace(')', '')
+                                            var targetX = d.target.attr("transform").split('(')[1].split(',')[0]
+                                            var targetY = d.target.attr("transform").split('(')[1].split(',')[1].replace(')', '')
+                                            return "M" + sourceX + "," + sourceY
+                                            + "L" + targetX + "," + targetY;
+                                    });
+                            }
+                    }
+
+                // Color nodes on the way to the centre
+                var intermediateNode = d3.select("#a" + CryptoJS.SHA1(value.to))
+                    intermediateNode.style("fill", function(d) {
+                        if (d.name != artist_name) {
+                            return d3.rgb(255, 106, 71);
+                        } else {
+                            return d3.rgb(255, 0, 0);
+                        }
+                    })
+                intermediateNodes.push(intermediateNode)
+
+                    // Write out the path
+                    var sentence = "<p>"
+                    sentence += value.from
+                    sentence += " and "
+                    sentence += value.to
+                    sentence += " collaborated on "
+                    sentence += "<em>" + value.song + "</em>"
+                    sentence += "</p>"
+                    yeezyPath.append($(sentence))
+            })
         }
 }
 
@@ -207,48 +210,58 @@ vis.attr("transform",
 svg.call(zoom)
     .on("dblclick.zoom", null)
 
-    d3.json("yeezified_network.json", function(error, graph) {
-
+    d3.json("yeezy_network.json", function(error, graph) {
+        yeezy_only = true;
+        console.log("Loaded yeezied");
         thresholdValue = 10;
         artist_name = "Kanye West";
+        makeGraph(graph, thresholdValue, artist_name);
+    });
 
-        $('#autocomplete').autocomplete({
-            source: function(request, response) {
-                var results = $.ui.autocomplete.filter(artist_names, request.term);
-                response(results.slice(0, 4));
-            },
-            select : function(event, ui) {
-                artist_name = ui.item.value;
-                makeGraph(graph, thresholdValue, artist_name);
-                artistName.focus();
-            }
-        });
+d3.json("yeezified_network.json", function(error, graph) {
+    yeezy_only = false;
+    console.log("Loaded yeezified")
 
-        $('#autocomplete-second').autocomplete({
-            source: function(request, response) {
-                var results = $.ui.autocomplete.filter(artist_names, request.term);
-                response(results.slice(0, 4));
-            },
-            select: function(event, ui) {
-                second_artist_name = ui.item.value;
-                for (var nodeIndex = 0, l = graph.nodes.length; nodeIndex < l; nodeIndex ++) {
-                    var d = graph.nodes[nodeIndex];
-                    if (d.name == second_artist_name) {
-                        highlightNode(d, d3.select("#a" + CryptoJS.SHA1(second_artist_name)))
-                    }
+        thresholdValue = 10;
+    artist_name = "Kanye West";
+
+    $('#autocomplete').autocomplete({
+        source: function(request, response) {
+            var results = $.ui.autocomplete.filter(artist_names, request.term);
+            response(results.slice(0, 4));
+        },
+        select : function(event, ui) {
+            artist_name = ui.item.value;
+            makeGraph(graph, thresholdValue, artist_name);
+            artistName.focus();
+        }
+    });
+
+    $('#autocomplete-second').autocomplete({
+        source: function(request, response) {
+            var results = $.ui.autocomplete.filter(artist_names, request.term);
+            response(results.slice(0, 4));
+        },
+        select: function(event, ui) {
+            second_artist_name = ui.item.value;
+            for (var nodeIndex = 0, l = graph.nodes.length; nodeIndex < l; nodeIndex ++) {
+                var d = graph.nodes[nodeIndex];
+                if (d.name == second_artist_name) {
+                    highlightNode(d, d3.select("#a" + CryptoJS.SHA1(second_artist_name)))
                 }
             }
-        });
+        }
+    });
 
-        d3.select('#threshold-slider').call(d3.slider()
-                .value(thresholdValue)
-                .on("slide", function(evt, value) {
-                    thresholdValue = value;
-                    makeGraph(graph, value, artist_name);
-                }))
+    d3.select('#threshold-slider').call(d3.slider()
+            .value(thresholdValue)
+            .on("slide", function(evt, value) {
+                thresholdValue = value;
+                makeGraph(graph, value, artist_name);
+            }))
 
-        makeGraph(graph, thresholdValue, artist_name);
-    })
+    makeGraph(graph, thresholdValue, artist_name);
+})
 
 function makeGraph(graph, value, artist_name) {
     vis.selectAll('*').remove();
